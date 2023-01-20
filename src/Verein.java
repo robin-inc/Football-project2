a534aa98b0e3718dfc0b8649d48a0a65ba4deccf
Robin, Kuehn <kuehn.robin@gmx.net>
2023-01-20 13:10:45 +0100

    Verein Klasse

---
+++ src/Verein.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Verein {
+    public String name;
+    private int gruendungsjahr;
+    private double vermoegen;
+    public int rating;
+    public Stadion stadion;
+    public Trikot trikot;
+    public Mannschaft mannschaft;
+
+    public Verein() {}
+
+    public void Information() //zeigt Informationen über den Verein an
+    {
+        System.out.println("++++++++++++++++++++");
+        System.out.println("Vereinsname: " + name);
+        System.out.println("Gründungsjahr: " + gruendungsjahr);
+        System.out.println("Stadion: " + stadion);
+        System.out.println("Aktuelles Vereinsvermögen: " + vermoegen);
+        System.out.println("++++++++++++++++++++");
+    }
+
+    public static void Compare(Verein a, Verein b) //vergleicht zwei angegebene Vereine miteinander
+    {
+        if (a.gruendungsjahr < b.gruendungsjahr)
+        {System.out.println("Der Verein " + a.name + " ist älter als " + b.name + ".");}
+        else {System.out.println("Der Verein " + b.name + " ist älter als " + a.name + ".");}
+        if (a.vermoegen > b.vermoegen){System.out.println("Der Verein " + a.name + " ist wohlhabender als " + b.name + ".");}
+        else {System.out.println("Der Verein " + b.name + " ist wohlhabender als " + a.name + ".");}
+    }
+
+    public void addToList(Verein e,ArrayList<Verein> verein_liste)
+    {verein_liste.add(e);}
+
+    public void removeFromList(Verein e, ArrayList<Verein> verein_liste)
+    {verein_liste.remove(e);}
+
+    public static void Register(ArrayList<Verein> verein_liste)
+    {
+        System.out.println("+++REGISTER+++\n" + verein_liste.toString());
+    }
+
+
+
+
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public double getVermoegen() {return vermoegen;}
+    public void setVermoegen(double vermoegen) {this.vermoegen = vermoegen;}
+    public int getGruendungsjahr() {return gruendungsjahr;}
+    public void setGruendungsjahr(int gruendungsjahr) {this.gruendungsjahr = gruendungsjahr;}
+    public int getRating() {return rating;}
+    public void setRating(int rating) {this.rating = rating;}
+    public Stadion getStadion() {return stadion;}
+    public void setStadion(Stadion stadion) {this.stadion = stadion;}
+    public Trikot getTrikot() {return trikot;}
+    public void setTrikot(Trikot trikot) {this.trikot = trikot;}
+    public Mannschaft getMannschaft() {return mannschaft;}
+    public void setMannschaft(Mannschaft mannschaft) {this.mannschaft = mannschaft;}
+}
