dd3861a6975c7ff288fe9c35e50fb2c2d96bb97a
Lukas.Tolopilo <lukas.tolopilo@accenture.com>
2023-01-20 13:38:20 +0100

    Änderung Morton

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Morton!");
+
+        Personal x = new Personal("Ulle", 33, 8.7, "Spieler", false, false);
+        Personal y = new Personal("Ibra", 39, 10.3, "Spieler", true, false);
+        Personal z = new Personal("Rashford", 23, 100, "Spieler", false, false);
+
+        x.status();
+        System.out.println();
+        y.status();
+        System.out.println();
+        System.out.println();
+
+
+        Personal.Compare(x, y);
+        System.out.println();
+        Personal.Compare(x, z);
+
     }
 }
